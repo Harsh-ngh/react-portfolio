6deab58a64877dba78b05eb31a549ba158fa08b3
Harsh Singh <sengarharshsinghrajput@gmail.com>
2024-07-14 18:14:04 +0530

    contact update

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_qkzfgem', 'template_nzyb3rs', form.current, '0F3YlDzMBZQL92VU4')
       .then((result) => {
         setOpen(true);
         form.current.reset();
@@ -147,7 +147,7 @@ const Contact = () => {
         <Title>Contact</Title>
         <Desc>Feel free to reach out to me for any questions or opportunities!</Desc>
         <ContactForm ref={form} onSubmit={handleSubmit}>
-          <ContactTitle>Email Me 🚀</ContactTitle>
+          <ContactTitle>Contact Me</ContactTitle>
           <ContactInput placeholder="Your Email" name="from_email" />
           <ContactInput placeholder="Your Name" name="from_name" />
           <ContactInput placeholder="Subject" name="subject" />
